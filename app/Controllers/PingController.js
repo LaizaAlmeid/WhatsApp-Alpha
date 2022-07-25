e7598ce7ae1ced777e986d191e8446e97dea0d50
Laiza Almeida <laiza1606@gmail.com>
2022-07-25 15:54:52 -0300

    25-07*15:54 -p commit

--- app/Controllers/PingController.js
+++ app/Controllers/PingController.js
@@ -2,11 +2,12 @@ class PingController {
 
   async index(req, res) {
     const { status } = req.body;
-   
+    console.log("SERVICO ATIVO")
     return res.status(200).json({
         status: req.body
       }
-      )
+    )
+    
   }
 }
 
